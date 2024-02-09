adab6982b918e25e55b20764eb76406624bc0c2b
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-02-09 23:14:47 +0100

    (chore) Refactors js side with jQuery - table is using bootstrapToggles

--- collapse.js
+++ collapse.js
@@ -1,75 +1,64 @@
 // Example of call of the function to make the table tree-like:
 // tableToTree('#myTable', 'opened', 'visible', '<span class="toggle"></span>');
+// after refactoring it only needs the jquery row selector
 
-function tableToTree(table_Selector, tr_OpenedClass, tr_VisibleClass, tr_ToggleButton) {
+function tableToTree($rowSelector) {
+	$rowSelector.each(function(_, tr) {
+		let $tr = $(tr)
+		let level = parseInt($tr.attr("level"))
+		if (isNaN(level)) {
+			return true;
+		}
+		let $nextRow = $tr.closest('tr').next('tr')
+		let nextLevel = parseInt($nextRow.attr("level"))
 
-	// Table elements variables
-	var table = document.querySelector(table_Selector);
-	var trs = document.querySelectorAll(table_Selector + " tr");
-
-	// Add the buttons above the table
-	var buttons = document.createElement('div');
-	buttons.innerHTML = '<button>[‒] All</button><button>[+] All</button>';
-	table.insertBefore(buttons, table.childNodes[0]);
-	buttons = buttons.querySelectorAll('button');
-	// Add the actions of these buttons
-	buttons[0].addEventListener("click", function() {
-		trs.forEach(function(elm) {
-			elm.classList.remove(tr_OpenedClass);
-			elm.classList.remove(tr_VisibleClass);
-		});
-	});
-	buttons[1].addEventListener("click", function() {
-		trs.forEach(function(elm) {
-			if (elm.innerHTML.includes(tr_ToggleButton))
-				elm.classList.add(tr_OpenedClass);
-			elm.classList.add(tr_VisibleClass);
-		});
-	});
-
-	function nextTr(row){
-		while ((row=row.nextSibling) && row.nodeType != 1);
-		return row
-	}
-
-	trs.forEach(function(tr, index){
-		if (index < trs.length - 1) {
-			if (+tr.getAttribute("level") < +trs[index + 1].getAttribute("level")) {
-				var td1 = tr.firstElementChild;
-				td1.innerHTML = tr_ToggleButton + td1.innerHTML
-			}
+		let $td1 = $tr.find('td:first')
+		let $toggleHolder = $("<span>").attr("class", "toggle-holder")
+		let $smToggle = $("<input>")
+			.attr("type", "checkbox")
+			.attr("data-size", "mini")
+			.addClass("row-toggle")
+		$toggleHolder.append($smToggle)
+		$td1.prepend($toggleHolder)
+		if ((nextLevel - level) === 1) {
+			$smToggle
+			.attr("data-off", "+")
+			.attr("data-on", "-")
+			.attr("data-offstyle", "danger")
+			.attr("data-onstyle", "success").bootstrapToggle("off")
+		} else {
+			$smToggle.attr("data-off", "x").bootstrapToggle("disable")
+		}
+		if (level === 0) {
+			$smToggle.addClass("root-toggle")
 		}
 	})
 
-	table.addEventListener("click", function(e){
-		e.preventDefault()
-		if (e.target.outerHTML != tr_ToggleButton) {
-			return;
-		}
-		let row = e.target.closest('tr')
-
-		let isCloseAction = row.classList.contains(tr_OpenedClass)
-		row.classList.toggle(tr_OpenedClass)
-		let lvl = +(row.getAttribute("level"));
-
-		while ((row = nextTr(row)) && +(row.getAttribute("level")) > lvl) {
-			if (isCloseAction) {
-				if (row.innerHTML.includes(tr_ToggleButton)) {
-					row.classList.remove(tr_OpenedClass)
-				}
-				if (row.classList.contains(tr_VisibleClass)) {
-					row.classList.remove(tr_VisibleClass)
-				}
-			} else {
-				if (row.innerHTML.includes(tr_ToggleButton)) {
-					row.classList.add(tr_OpenedClass)
-				}
-				if (!row.classList.contains(tr_VisibleClass)) {
-					row.classList.add(tr_VisibleClass)
-				}
+	$('.row-toggle').change(function() {
+		console.log("runs again")
+		let isOpenAction = $(this).prop('checked')
+		let $tr = $(this).closest('tr')
+		let level = parseInt($tr.attr("level"))
+		$tr.nextAll().each(function(_, tr) {
+			let $currentTr = $(tr)
+			let currentLevel = parseInt($currentTr.attr('level'))
+			if (currentLevel === level) {
+				return false;
 			}
-		}
+			let levelDiff = currentLevel - level
+			let $rowToggle = $currentTr.find('.row-toggle')
+			if (isOpenAction && levelDiff ===1 ) {
+				$currentTr.show()
+			} else if (!isOpenAction && levelDiff === 1) {
+				$currentTr.hide()
+				$rowToggle.bootstrapToggle('off')
+			}
+			return true
+		})
 	})
 }
 
-tableToTree("#myTable", 'opened', 'visible', '<span class="toggle"></span>')
+
+
+let $rowSelector = $("#myTable tr")
+tableToTree($rowSelector)
